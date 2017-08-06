0c574667409575ec35d8b5fb722988ddc54b8f2f
Administrator <Administrator@XB-201707232021>
2017-08-06 19:05:20 +0800

    aad

--- src/activity/MainActivity.java
+++ src/activity/MainActivity.java
@@ -1,21 +1,30 @@
 package activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
 import com.example.sharelife.R;
 
- import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
 public class MainActivity extends Activity {
+	
+
+
+
+	private double latitude = 0.0;
+	private double longitude = 0.0;
+
+
+
+	public AMapLocationClient mLocationClient = null;
+	public AMapLocationClientOption mLocationOption = null;
 	MapView mMapView = null;
 
 	@Override
@@ -32,6 +41,27 @@ public class MainActivity extends Activity {
 		if (aMap == null) {
 		    aMap = mMapView.getMap();        
 		}
+		
+		// 声明定位回调监听器
+
+		// 初始化定位
+		mLocationClient = new AMapLocationClient(getApplicationContext());
+		// 设置定位回调监听
+		mLocationClient.setLocationListener(mAMapLocationListener);
+
+		// 初始化AMapLocationClientOption对象
+		mLocationOption = new AMapLocationClientOption();
+
+		mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+
+		mLocationOption.setOnceLocationLatest(true);
+		mLocationOption.setNeedAddress(true);
+
+		mLocationClient.setLocationOption(mLocationOption);
+		// 启动定位
+		mLocationClient.startLocation();
+		
+		
 	}
 
 	@Override
@@ -63,4 +93,28 @@ public class MainActivity extends Activity {
 		mMapView.onSaveInstanceState(outState);
 	}
 
+	
+	AMapLocationListener mAMapLocationListener = new AMapLocationListener() {
+		@Override
+		public void onLocationChanged(AMapLocation amapLocation) {
+
+			if (amapLocation != null) {
+				if (amapLocation.getErrorCode() == 0) {
+					double latitude1 = amapLocation.getLatitude();
+					double longitude1 = amapLocation.getLongitude();
+					latitude = latitude1;
+					longitude = longitude1;
+					Log.i("latitude", latitude1 + "");
+					Log.i("longitude", longitude1 + "");
+
+				} else {
+					// 定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+					Log.e("AmapError", "location Error, ErrCode:"
+							+ amapLocation.getErrorCode() + ", errInfo:"
+							+ amapLocation.getErrorInfo());
+				}
+			}
+		}
+
+	};
 }
